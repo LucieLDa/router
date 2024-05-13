a2e0b1e15c746f1c9a5cffe50430002ece10c9ef
LuDanis <lucie.lindanis@gmail.com>
2024-05-13 14:56:51 +0200

    Add user interface and sign-up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  newUser: User = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: ""
+  }
 
+  onSubmit(): void {
+    console.log(this.newUser);
+  }
 }
